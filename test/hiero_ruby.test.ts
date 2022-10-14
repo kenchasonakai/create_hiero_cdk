b8deb3f47851821fe8e506ef4e21c94d270201ab
kenchaso <kenchasonakai@gmail.com>
2022-10-14 17:16:05 +0900

    Initial commit

---
+++ test/hiero_ruby.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HieroRuby from '../lib/hiero_ruby-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hiero_ruby-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HieroRuby.HieroRubyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
